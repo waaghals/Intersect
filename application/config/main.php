220c4185bfe372c65584f9099449bfe8b0e814df
Waaghals <patrick.stroef@gmail.com>
2012-03-13 23:48:19 +0100

    Create config file & image controller

---
+++ application/config/main.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['max_image_size']	= 5242880;
+$config['max_url_size'] 	= 255;
+$config['max_words'] 		= 100;
+$config['max_word_length'] 	= 10;
+
+
+/* End of file main.php */
+/* Location: ./application/config/main.php */
