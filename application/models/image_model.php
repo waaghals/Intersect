fd2a4387695244a300bfd0a2bdf00c545171a15f
Waaghals <patrick.stroef@gmail.com>
2012-03-14 23:11:15 +0100

    Added best images controller

--- application/models/image_model.php
+++ application/models/image_model.php
@@ -14,4 +14,34 @@ class Image_model extends CI_Model {
 		$path = $row->datetime . '/' . $row->digest . '.' . $row->extension;
 		return array('path' => $path, 'id' => $row->id);
 	}
+	
+	public function best($number) {
+		return $this->images($number, FALSE);
+	}
+	
+	public function worst($numer) {
+		return $this->images($number, TRUE);
+	}
+	
+	private function images($number, $worst) {
+		//Order asc when worst, desc when best
+		$order = ($worst) ? 'desc': 'asc';
+			
+		$sql = "
+			SELECT 
+				i.id, 
+				CONCAT( replace( CONVERT( datetime, date ) , '-', '/' ) , '/', digest, '.', extension ) AS path, 
+				d.width * d.height /150000 + i.rating AS rating
+			FROM image AS i
+				JOIN image_data AS d 
+					ON ( i.id = d.image_id ) 
+			ORDER BY score ?
+			LIMIT 0 , ?";
+			
+		if ($q->num_rows() > 0)
+		{
+			return $q->result();
+		}
+		return FALSE;
+	}
 }
