396fa6b4d7ca712f529e605c4060e0066f4a2c03
Waaghals <patrick.stroef@gmail.com>
2012-03-25 22:53:12 +0200

    Find similar images

--- application/models/signature_model.php
+++ application/models/signature_model.php
@@ -1,7 +1,7 @@
 <?php
 class Signature_model extends CI_Model {
 	
-	function split_into_words($sig) {
+	private function split_into_words($sig) {
 	    $words = array();
 	    $u = 0;    
 	    do {
@@ -11,7 +11,7 @@ class Signature_model extends CI_Model {
 	    return $words;
 	}
 	
-	function save_signature($md5, $cvec, $extension) {
+	public function save($md5, $cvec, $extension) {
 	    $compressed_cvec = puzzle_compress_cvec($cvec);
 	    $words = $this->split_into_words($cvec);
 		
@@ -42,5 +42,44 @@ class Signature_model extends CI_Model {
 			$this->db->query($sql, array(chr($u) . puzzle_compress_cvec($word), $signature_id));
 		}
 		$this->db->trans_complete();
+	}
+
+	public function similar($md5, $cvec, $threshold = PUZZLE_CVEC_SIMILARITY_THRESHOLD) {
+
+	    $words = $this->split_into_words($cvec);
+		$sql = 'SELECT compressed_signature, image_id 
+				FROM signature
+				JOIN word 
+					ON word.signature.id = signature.id
+				WHERE pos_and_word 
+					IN (' . $this->implode_words($words) . ')';
+		$query = $this->db->query($sql);
+		
+		if ($query->num_rows() > 0) {
+			$scores = array();
+			
+		    foreach ($query->result() as $row) {
+	       		$uncompressed_cvec = puzzle_uncompress_cvec($row->compressed_signature);
+				$distance = puzzle_vector_normalized_distance($cvec, $uncompressed_cvec);
+				
+	       		if ($distance < $threshold && $distance > 0.0) {
+			    	$scores[$picture_id] = $distance;
+				}
+	    	}
+		}
+	    return $scores;
+	}
+
+	private function implode_words($words) {
+		$glue = FALSE;
+	    foreach ($words as $u => $word) {
+			if ($glue === TRUE) {
+			    $str .= ',';
+			}
+			
+			$str .= chr($u) . puzzle_compress_cvec($word);
+			$coma = TRUE;
+	    }
+		return $str;
 	}
 }
