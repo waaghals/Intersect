075d63ba1ac8ee8d2d866703fd50be1573cbd6d5
Waaghals <patrick.stroef@gmail.com>
2012-03-30 14:11:15 +0200

    Move the top X images list to the view controller

--- controllers/view.php
+++ controllers/view.php
@@ -7,4 +7,24 @@ class View extends CI_Controller {
 		$img = $this->image->get_image($id);
 		$this->load->view('image', $img);
 	}
+	
+	public function top($number = 500) {
+		$this->load->model('images_model', 'images');
+		
+		foreach($this->images->best($number) as $image) {
+			$data = array('path' => $image->path, 'id' => $image->id);
+			$this->load->view('image', $data);
+		}
+		$this->load->view('tracker');
+	}
+	
+	public function percentile() {
+		$this->load->model('images_model', 'images');
+		
+		foreach($this->images->percentile() as $image) {
+			$data = array('path' => $image->path, 'id' => $image->id);
+			$this->load->view('image', $data);
+		}
+		$this->load->view('tracker');
+	}
 }
