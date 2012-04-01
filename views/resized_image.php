168e6f4c9b0a66509c8c90d7b40cf8a892464240
Waaghals <patrick.stroef@gmail.com>
2012-04-01 23:24:02 +0200

    Removed index.php from image path, .htaccess handles that

--- views/resized_image.php
+++ views/resized_image.php
@@ -1,5 +1,5 @@
 <div style="float: left; margin: 3px; padding: 0px; position: relative;">
 	<div style="width: <?php echo $vwidth; ?>px; height: <?php echo $theight; ?>px; overflow-x: hidden; overflow-y: hidden">
-		<img src="/index.php/image/resize/<?php echo $id; ?>/<?php echo $twidth; ?>/<?php echo $theight; ?>" style="width: <?php echo $twidth; ?>px; height: <?php echo $theight; ?>px; margin-left: <?php echo ($vwidth-$twidth)/2; ?>px margin-top: 0px"/>
+		<img src="/image/resize/<?php echo $id; ?>/<?php echo $twidth; ?>/<?php echo $theight; ?>" style="width: <?php echo $twidth; ?>px; height: <?php echo $theight; ?>px; margin-left: <?php echo ($vwidth-$twidth)/2; ?>px margin-top: 0px"/>
 	</div>
 </div>
