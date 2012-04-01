e6e3567c7e36eedf6298f8585e4f393e92e55f2e
Waaghals <patrick.stroef@gmail.com>
2012-04-01 16:57:58 +0200

    Code optimization

--- models/signature_model.php
+++ models/signature_model.php
@@ -3,32 +3,30 @@ class Signature_model extends CI_Model {
 	
 	private function split_into_words($sig) {
 	    $words = array();
-	    $u = 0;    
-	    do {
-			$words[$u] = substr($sig, $u, $this->config->item('max_words_length'));
-	    } while (++$u < $this->config->item('max_words'));
-	
+
+		for ($i=0; $i < 100; $i++) { 
+			$words[] = substr($sig, $i, $this->config->item('max_word_length'));
+		}
+
 	    return $words;
 	}
 	
-	public function save($md5, $cvec, $extension) {
+	public function save($md5, $cvec) {
 	    $compressed_cvec = puzzle_compress_cvec($cvec);
 	    $words = $this->split_into_words($cvec);
 		
-		$this->db->trans_start();
 		$sql = "SELECT id FROM image WHERE digest = ?"; 
 		$r = $this->db->query($sql, array($md5));
 		if ($r->num_rows() > 0) {
 			$row = $r->row(); 
-			$image_id = $row->id;
 			
-			$this->db->trans_complete();
-			return TRUE;
+			//It is a exact duplicate based on the md5
+			return $row->id;
 		}
 		
 		//No duplicate found
-		$sql = "INSERT INTO image (digest, extension, datetime) VALUES (?, ?, ?)";
-		$this->db->query($sql, array($md5, $extension, date("Y-m-d H:i:s")));
+		$sql = "INSERT INTO image (digest, uploaded) VALUES (?, ?)";
+		$this->db->query($sql, array($md5, date("Y-m-d H:i:s")));
 		$image_id = $this->db->insert_id();
 		
 		//Insert the new signature in the db
@@ -37,11 +35,12 @@ class Signature_model extends CI_Model {
 		$signature_id = $this->db->insert_id();
 		
 		//Insert all the words
-		$sql = "INSERT INTO word (pos_and_word, signature_id) VALUES (:pos_and_word, :signature_id)";
 		foreach ($words as $u => $word) {
-			$this->db->query($sql, array(chr($u) . puzzle_compress_cvec($word), $signature_id));
+			$data[] = array('pos_and_word' => chr($u) . puzzle_compress_cvec($word), 'signature_id' => $signature_id);
 		}
-		$this->db->trans_complete();
+		$this->db->insert_batch('word', $data);
+
+		return $image_id;
 	}
 
 	public function similar($md5, $cvec, $threshold = PUZZLE_CVEC_SIMILARITY_THRESHOLD) {
