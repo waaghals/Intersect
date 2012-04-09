b6e27b04089652f62458eed6b57fc9778e97867e
Waaghals <patrick.stroef@gmail.com>
2012-04-09 19:08:32 +0200

    Reorder folder structure for easy deployment

--- application/config/main.php
+++ application/config/main.php
@@ -4,7 +4,7 @@ $config['max_image_size']	= 5242880;
 $config['max_url_size'] 	= 255;
 $config['max_word_length'] 	= 10;
 $config['max_words'] 		= 500;
-$config['img_dir']			= APPPATH . 'images';
+$config['img_dir']			= 'images';
 
 
 /* End of file main.php */
