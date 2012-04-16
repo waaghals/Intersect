db51c2bef822f6fa06e9c2eb00d20f8a60891020
Waaghals <patrick.stroef@gmail.com>
2012-04-16 22:22:25 +0200

    Moved alert boxes in main content container

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -26,30 +26,3 @@
 
   <body data-spy="scroll" data-target=".subnav" data-offset="50">
   <br /><br /><br />
-  <?php if($this->session->flashdata('warning') != ''): ?>
-	<div class="alert warning">
-	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Warning!</strong> <?php echo $this->session->flashdata('warning');?>
-	</div>
-  <?php endif;?>
-  
-  <?php if($this->session->flashdata('notice') != ''): ?>
-	<div class="alert alert-info">
-	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Notice</strong> <?php echo $this->session->flashdata('notice');?>
-	</div>
-  <?php endif;?>
-  
-  <?php if($this->session->flashdata('error') != ''): ?>
-	<div class="alert alert-error">
-	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Error!</strong> <?php echo $this->session->flashdata('error');?>
-	</div>
-  <?php endif;?>
-  
-  <?php if($this->session->flashdata('success') != ''): ?>
-	<div class="alert alert-success">
-	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Success</strong> <?php echo $this->session->flashdata('success');?>
-	</div>
-  <?php endif;?>
