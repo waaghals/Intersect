eb8ac09c212bb2392103e5c0bb41c221f8781253
Waaghals <patrick.stroef@gmail.com>
2012-04-16 14:52:12 +0200

    New css styles, from bootswatch

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,9 +8,8 @@
     <meta name="author" content="Waaghals">
 
     <!-- Le styles -->
-    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/cerulean.css" rel="stylesheet">
     <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="/assets/css/docs.css" rel="stylesheet">
     <link href="/assets/js/google-code-prettify/prettify.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -30,27 +29,27 @@
   <?php if($this->session->flashdata('warning') != ''): ?>
 	<div class="alert warning">
 	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Warning!</strong> <?php echo $this->session->flashdata('warning'); ?>
+	  <strong>Warning!</strong> <?php echo $this->session->flashdata('warning');?>
 	</div>
-  <?php endif; ?>
+  <?php endif;?>
   
   <?php if($this->session->flashdata('notice') != ''): ?>
 	<div class="alert alert-info">
 	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Notice</strong> <?php echo $this->session->flashdata('notice'); ?>
+	  <strong>Notice</strong> <?php echo $this->session->flashdata('notice');?>
 	</div>
-  <?php endif; ?>
+  <?php endif;?>
   
   <?php if($this->session->flashdata('error') != ''): ?>
 	<div class="alert alert-error">
 	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Error!</strong> <?php echo $this->session->flashdata('error'); ?>
+	  <strong>Error!</strong> <?php echo $this->session->flashdata('error');?>
 	</div>
-  <?php endif; ?>
+  <?php endif;?>
   
   <?php if($this->session->flashdata('success') != ''): ?>
 	<div class="alert alert-success">
 	  <a class="close" data-dismiss="alert">×</a>
-	  <strong>Success</strong> <?php echo $this->session->flashdata('success'); ?>
+	  <strong>Success</strong> <?php echo $this->session->flashdata('success');?>
 	</div>
-  <?php endif; ?>
+  <?php endif;?>
