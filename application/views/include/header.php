27cf28e8ebada3e149fe7294e9802691cecc4aed
Waaghals <patrick.stroef@gmail.com>
2012-04-18 16:03:51 +0200

    Changes style to bootstrap again

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Waaghals">
 
     <!-- Le styles -->
-    <link href="/assets/css/cerulean.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
     <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="/assets/js/google-code-prettify/prettify.css" rel="stylesheet">
 
