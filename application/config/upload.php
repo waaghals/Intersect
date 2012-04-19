6ae7eaed7ff065273c726a4767e3200ff6813515
Waaghals <patrick.stroef@gmail.com>
2012-04-19 18:59:55 +0200

    Allow larger files

--- application/config/upload.php
+++ application/config/upload.php
@@ -1,7 +1,7 @@
 <?php
 $config['upload_path'] 		= APPPATH . '/tmp/';
 $config['allowed_types'] 	= 'gif|jpg|png';
-$config['max_size']			= '10240'; //10MB
-$config['max_width'] 		= '5000';
-$config['max_height']  		= '5000';
+$config['max_size']			= '20480'; //20MB
+$config['max_width'] 		= '6000';
+$config['max_height']  		= '6000';
 $config['overwrite'] 		= FALSE;
